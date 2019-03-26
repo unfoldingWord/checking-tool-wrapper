0357da64b6b66bd893d87d6ff5ae4af3f7d639c2
mannycolon <colonmanuel7@gmail.com>
2019-03-26 00:13:19 -0400

    Testing different approach

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@ export { default as Api } from './Api';
 export { default as ToolWrapper } from './ToolWrapper';
 // export { default as reducers } from './reducers';
 
+import {connectTool} from 'tc-tool';
+// import path from 'path';
+
+export default connectTool('translationWords', {
+  // localeDir: path.join(__dirname, './src/locale'),
+  api: new Api()
+})(ToolWrapper);
