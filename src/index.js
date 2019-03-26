c8ea91295cdcd4436b293e8dffd4e3b309ed7388
mannycolon <colonmanuel7@gmail.com>
2019-03-26 01:01:00 -0400

    ADDED locale

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
+import {connectTool} from 'tc-tool';
+import path from 'path';
 import Api from './Api';
 import ToolWrapper from './ToolWrapper';
 // export { default as reducers } from './reducers';
 
-import {connectTool} from 'tc-tool';
-// import path from 'path';
 
 export default connectTool('translationWords', {
-  // localeDir: path.join(__dirname, './src/locale'),
+  localeDir: path.join(__dirname, './src/locale'),
   api: new Api()
 })(ToolWrapper);
