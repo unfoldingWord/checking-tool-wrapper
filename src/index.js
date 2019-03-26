6257219982dfa07947976ab7ea2a3fde1ddd750c
mannycolon <colonmanuel7@gmail.com>
2019-03-26 01:13:51 -0400

    removed locale

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
-import {connectTool} from 'tc-tool';
-import path from 'path';
-import Api from './Api';
-import ToolWrapper from './ToolWrapper';
+export { default as Api } from './Api';
+export { default as ToolWrapper } from './ToolWrapper';
 // export { default as reducers } from './reducers';
 
-
-export default connectTool('translationWords', {
-  localeDir: path.join(__dirname, './src/locale'),
-  api: new Api()
-})(ToolWrapper);
