9f36ecff94aaea36cf8f465b3864f35ed9a12891
mannycolon <colonmanuel7@gmail.com>
2019-09-18 01:19:45 -0400

    testing useEffect

--- src/components/TranslationHelpsWrapper.js
+++ src/components/TranslationHelpsWrapper.js
@@ -1,7 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function TranslationHelpsWrapper() {
   const [greeting, setGreeting] = useState('hello');
+
+  useEffect(() => {
+    setGreeting('hello2');
+  }, [greeting]);
 
   return <div onClick={() => setGreeting('Hey')}>
     <span>{greeting} world!</span>
