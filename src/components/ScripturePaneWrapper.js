f6be5ecd2621c54a77974cfa8e077196c3cc3363
mannycolon <colonmanuel7@gmail.com>
2019-09-23 16:04:33 -0400

    Converted ScripturePaneWrapper.js to functional component

--- src/components/ScripturePaneWrapper.js
+++ src/components/ScripturePaneWrapper.js
@@ -2,53 +2,50 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { ScripturePane } from 'tc-ui-toolkit';
 
-class ScripturePaneWrapper extends React.Component {
-  makeTitle(manifest) {
-    const { target_language, project } = manifest;
+function makeTitle(manifest) {
+  const { target_language, project } = manifest;
 
-    if (target_language && target_language.book && target_language.book.name) {
-      return target_language.book.name;
-    } else {
-      return project.name;
-    }
+  if (target_language && target_language.book && target_language.book.name) {
+    return target_language.book.name;
+  } else {
+    return project.name;
   }
+}
 
-  render() {
-    const {
-      manifest,
-      showPopover,
-      editTargetVerse,
-      projectDetailsReducer,
-      getLexiconData,
-      selections,
-      setToolSettings,
-      bibles,
-      contextId,
-      translate,
-      currentPaneSettings,
-      getAvailableScripturePaneSelections,
-      makeSureBiblesLoadedForTool,
-    } = this.props;
+function ScripturePaneWrapper({
+  manifest,
+  showPopover,
+  editTargetVerse,
+  projectDetailsReducer,
+  getLexiconData,
+  selections,
+  setToolSettings,
+  bibles,
+  contextId,
+  translate,
+  currentPaneSettings,
+  getAvailableScripturePaneSelections,
+  makeSureBiblesLoadedForTool,
+}) {
+  const expandedScripturePaneTitle = makeTitle(manifest);
 
-    const expandedScripturePaneTitle = this.makeTitle(manifest);
-    return (
-      <ScripturePane
-        currentPaneSettings={currentPaneSettings}
-        contextId={contextId}
-        bibles={bibles}
-        expandedScripturePaneTitle={expandedScripturePaneTitle}
-        showPopover={showPopover}
-        editTargetVerse={editTargetVerse}
-        projectDetailsReducer={projectDetailsReducer}
-        translate={translate}
-        getLexiconData={getLexiconData}
-        selections={selections}
-        setToolSettings={setToolSettings}
-        getAvailableScripturePaneSelections={getAvailableScripturePaneSelections}
-        makeSureBiblesLoadedForTool={makeSureBiblesLoadedForTool}
-      />
-    );
-  }
+  return (
+    <ScripturePane
+      currentPaneSettings={currentPaneSettings}
+      contextId={contextId}
+      bibles={bibles}
+      expandedScripturePaneTitle={expandedScripturePaneTitle}
+      showPopover={showPopover}
+      editTargetVerse={editTargetVerse}
+      projectDetailsReducer={projectDetailsReducer}
+      translate={translate}
+      getLexiconData={getLexiconData}
+      selections={selections}
+      setToolSettings={setToolSettings}
+      getAvailableScripturePaneSelections={getAvailableScripturePaneSelections}
+      makeSureBiblesLoadedForTool={makeSureBiblesLoadedForTool}
+    />
+  );
 }
 
 ScripturePaneWrapper.propTypes = {
