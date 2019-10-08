5d208d903e2240ad60dc330d92d20a527e23080c
Richard Mahn <richmahn@users.noreply.github.com>
2019-10-08 21:20:52 +0800

    Refactor - #6445 - CheckInfoCardWrapper (#49)
    * Refactor - #6445 - CheckingInfoTool
    
    * Version bump
    
    * Version bump
    
    * Updates tests
    
    * Adds checkingInfoCardHelpers

--- __tests__/Container.test.js
+++ __tests__/Container.test.js
@@ -1,17 +1,19 @@
 /* eslint-env jest */
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect ,Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
-import {shallow, mount, render} from 'enzyme';
+import {
+ shallow, mount, render
+ } from 'enzyme';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import {Provider} from 'react-redux';
+
 import toJson from 'enzyme-to-json';
 // containers
 import PlainContainer, { mapStateToProps } from '../src/Container';
 import TranslationHelpsContainer from '../src/components/TranslationHelpsWrapper';
 import CheckInfoCardWrapper from '../src/components/CheckInfoCardWrapper';
-import {TRANSLATION_WORDS} from "../src/helpers/consts";
+import { TRANSLATION_WORDS } from '../src/helpers/consts';
 
 const Container = connect(mapStateToProps)(PlainContainer);
 
@@ -24,10 +26,8 @@ const props = {
   translate: k => k,
   tc: {
     ...basicProps.tc,
-    project: {
-      getBookName: () => () => "gen"
-    }
-  }
+    project: { getBookName: () => () => 'gen' },
+  },
 };
 
 props.tc.actions = {
@@ -46,16 +46,17 @@ props.tc.actions = {
   groupMenuChangeGroup: jest.fn(),
   groupMenuExpandSubMenu: jest.fn(),
   onInvalidCheck: jest.fn(),
-  getSelectionsFromContextId: () => ''
+  getSelectionsFromContextId: () => '',
 };
 
 const store = mockStore({});
+
 describe.only('Container Tests', () => {
   it('Test Container', () => {
     const wrapper = render(
-        <Provider store={store}>
-          <Container {...props} />
-        </Provider>
+      <Provider store={store}>
+        <Container {...props} />
+      </Provider>
     );
     expect(toJson(wrapper)).toMatchSnapshot();
   });
@@ -63,14 +64,12 @@ describe.only('Container Tests', () => {
   it('Test empty Container', () => {
     const myProps = {
       ...props,
-      contextIdReducer: {
-        contextId: null
-      }
+      contextIdReducer: { contextId: null },
     };
     const wrapper = renderer.create(
-        <Provider store={store}>
-          <Container {...myProps} />
-        </Provider>
+      <Provider store={store}>
+        <Container {...myProps} />
+      </Provider>
     );
     expect(toJson(wrapper)).toMatchSnapshot();
   });
@@ -102,43 +101,6 @@ describe.only('Container Tests', () => {
         <Container {...props} />
       </Provider>)
       .find(TranslationHelpsContainer);
-      expect(root).toBeTruthy();
-  });
-
-  it('Test CheckInfoCardWrapper.getNote() to remove (See: ...)', () => {
-    const props = {
-      showHelps: true,
-      toggleHelps: jest.fn(),
-      groupsIndex: [{id: 'figs-metaphor', name: 'Metaphor'}],
-      contextId: {
-        groupId: 'figs-metaphor',
-        occurrenceNote: 'Paul speaks of God’s message as if it were an object (not abstract) ([Titus 2:11](rc://en/ult/book/tit/02/11)) that could be visibly shown to people. Alternate translation: “He caused me to understand his message” (See: [Idiom](rc://en/ta/man/translate/figs-idiom), [[rc://some/unknown/link]] and [Metaphor](rc://en/ta/man/translate/figs-metaphor)) ',
-        tool: 'translationNotes',
-      },
-      resourcesReducer: {},
-      translate: k => k,
-    }
-    const checkInfoCardWrapper = shallow(<CheckInfoCardWrapper {...props} />);
-    const note = checkInfoCardWrapper.instance().getNote(props.contextId.occurrenceNote);
-    const expectedNote = 'Paul speaks of God’s message as if it were an object (not abstract) ([Titus 2:11](rc://en/ult/book/tit/02/11)) that could be visibly shown to people. Alternate translation: “He caused me to understand his message”';
-    expect(note).toEqual(expectedNote);
-  });
-
-  it('Test CheckInfoCardWrapper.getNote() where Bible verse at the end does NOT get removed, nothing should change', () => {
-    const props = {
-      showHelps: true,
-      toggleHelps: jest.fn(),
-      groupsIndex: [{id: 'figs-metaphor', name: 'Metaphor'}],
-      contextId: {
-        groupId: 'figs-metaphor',
-        occurrenceNote: 'Paul said this in another verse ([Titus 1:5](rc://en/ult/book/tit/01/05))',
-        tool: 'translationNotes',
-      },
-      resourcesReducer: {},
-      translate: k => k,
-    }
-    const checkInfoCardWrapper = shallow(<CheckInfoCardWrapper {...props} />);
-    const note = checkInfoCardWrapper.instance().getNote(props.contextId.occurrenceNote);
-    expect(note).toEqual(props.contextId.occurrenceNote);
+    expect(root).toBeTruthy();
   });
 });
---
+++ src/helpers/checkInfoCardHelpers.js
@@ -0,0 +1,55 @@
+
+/**
+ * Gets the phrase from tW
+ * @param {array} translationWords
+ * @param {string} articleId
+ * @param {object} translationHelps
+ * @return {string}
+ */
+export function getPhraseFromTw(translationWords, articleId, translationHelps) {
+  let currentFile = '';
+
+  if (translationWords && translationWords[articleId]) {
+    currentFile = translationHelps.translationWords[articleId];
+  }
+
+  let splitLine = currentFile.split('\n');
+
+  if (splitLine.length === 1 && splitLine[0] === '') {
+    return '';
+  }
+
+  let finalArray = [];
+
+  for (let i = 0; i < splitLine.length; i++) {
+    if (splitLine[i] !== '' && !~splitLine[i].indexOf('#')) {
+      finalArray.push(splitLine[i]);
+    }
+  }
+
+  let maxLength = 225;
+  let finalString = '';
+  let chosenString = finalArray[0];
+  let splitString = chosenString.split(' ');
+
+  for (let word of splitString) {
+    if ((finalString + ' ' + word).length >= maxLength) {
+      finalString += '...';
+      break;
+    }
+    finalString += ' ';
+    finalString += word;
+  }
+  return finalString;
+}
+
+/**
+ * Removes the (See: [...](rc://...)) links at the end of an occurrenceNote
+ * Ex: Paul speaks of God’s message as if it were an object (See: [Idiom](rc://en/ta/man/translate/figs-idiom) and [Metaphor](rc://en/ta/man/translate/figs-metaphor)) =>
+ *     Paul speaks of God’s message as if it were an object
+ * @param {string} occurrecntNote
+ * @return {string}
+ */
+export function getNote(occurrenceNote) {
+  return occurrenceNote.replace(/\s*\([^()[\]]+((\[[^[\]]+\])*(\[\[|\()+rc:\/\/[^)\]]+(\]\]|\))[^([)\]]*)+[^()[\]]*\)\s*$/g, '');
+}
