8d3c8ce1842dff37603793a9b9a8352bb62c7b99
mannycolon <colonmanuel7@gmail.com>
2019-10-11 18:21:04 -0400

    src/Container.js

--- src/Container.js
+++ src/Container.js
@@ -198,6 +198,6 @@ const mapStateToProps = (state, ownProps) => {
 console.log('__dirname', __dirname);
 
 export default connectTool('translationNotes', {
-  localeDir: path.join(__dirname, '/locale'),
+  // localeDir: path.join(__dirname, '/locale'),
   api: new Api(),
 })(connect(mapStateToProps)(Container));
