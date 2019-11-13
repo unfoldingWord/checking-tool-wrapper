38e39608500a9c122962ac3c37b12598929be419
mannycolon <colonmanuel7@gmail.com>
2019-11-13 16:13:08 -0400

    Init groupsIndexReducer & groupsIndexActions

---
+++ src/helpers/groupsIndexHelpers.js
@@ -0,0 +1,72 @@
+import fs from 'fs-extra';
+import path from 'path-extra';
+
+/**
+ * sorts either by chapter number, name, or id in that order
+ * @param a - first item to compare
+ * @param b - next item to compare
+ * @return {number}
+ */
+export function sortIndex(a, b) {
+  // if the id string contains chapter_ then remove it so that it doesnt mess up with the sorting
+  // otherwise it'd leave it alone
+  const A = a.id.includes('chapter_') ? parseInt(a.id.replace('chapter_', ''), 10) : (a.name || a.id).toLowerCase();
+  const B = b.id.includes('chapter_') ? parseInt(b.id.replace('chapter_', ''), 10) : (b.name || b.id).toLowerCase();
+
+  if (A < B) {
+    return -1;
+  }
+
+  if (A > B) {
+    return 1;
+  }
+  return 0;
+}
+
+/**
+ * Loads the groups index from the global resources.
+ * This is used primarily for generating the groups menu.
+ * This is boiler plate to keep a separation of concerns between the global resources and projects.
+ * TODO: the groups index should be copied into the project as part of {@link copyGroupDataToProject} and loaded from the project instead of the global resources.
+ * @param {string} gatewayLanguage - the gateway language code
+ * @param {string} toolName - the name of the tool who's index will be loaded
+ * @param {string} projectDir - path to the project directory
+ * @param {function} translate - the locale function. TODO: refactor index loading so locale is not required
+ * @return {*}
+ */
+export function loadProjectGroupIndex(
+  gatewayLanguage, toolName, projectDir, translate) {
+  const project = new ProjectAPI(projectDir);
+  const resources = ResourceAPI.default();
+  const helpDir = resources.getLatestTranslationHelp(gatewayLanguage, toolName);
+
+  if (helpDir) {
+    // load indices
+    const indices = [];
+    const categories = project.getSelectedCategories(toolName, true);
+
+    for (const categoryName in categories) {
+      const categoryIndex = path.join(helpDir, categoryName, 'index.json');
+
+      if (fs.lstatSync(categoryIndex).isFile()) {
+        try {
+          const selectedSubcategories = categories[categoryName];
+          // For categories with subcategories need to filter out not selected items.
+          const categoryIndices = fs.readJsonSync(categoryIndex)
+            .filter(item => selectedSubcategories.includes(item.id));
+          indices.push.apply(indices, categoryIndices);
+        } catch (e) {
+          console.error(`Failed to read group index from ${categoryIndex}`, e);
+        }
+      } else {
+        console.warn(`Unexpected tool category selection in ${projectDir}. "${categoryIndex}" could not be found.`);
+      }
+    }
+    return indices;
+  } else {
+    // generate indices
+    return generateChapterGroupIndex(translate);
+  }
+
+  // TODO: the export needs to have the groups index so we need to run this when selecting a tool _and_ when exporting.
+}
---
+++ src/state/actions/groupsIndexActions.js
@@ -0,0 +1,18 @@
+import { LOAD_GROUPS_INDEX, UPDATE_REFRESH_COUNT_GROUPS_INDEX } from './actionTypes';
+
+/**
+ * @description This action sends all of the group Ids and
+ * group names to the groupsIndexReducer
+ * @param {string} groupIndex - The object of group indecies
+ * @return {object} action object.
+ */
+export const loadGroupsIndex = (groupsIndex) => ((dispatch) => {
+  dispatch({
+    type: LOAD_GROUPS_INDEX,
+    groupsIndex,
+  });
+});
+
+export const updateRefreshCount = () => ((dispatch) => {
+  dispatch({ type: UPDATE_REFRESH_COUNT_GROUPS_INDEX });
+});
