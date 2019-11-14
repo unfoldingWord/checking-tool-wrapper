a5d7e4786d0fa5db7d511f1dda32909935ec539e
mannycolon <colonmanuel7@gmail.com>
2019-11-14 14:50:58 -0400

    Logging ownProps

--- src/Container.js
+++ src/Container.js
@@ -25,6 +25,7 @@ const theme = createTcuiTheme({
 
 function Container(props) {
   console.log('state', props.state);
+  console.log('ownProps', props.ownProps);
   const [showHelps, setShowHelps] = useState(true);
 
   useEffect(() => {
@@ -91,6 +92,7 @@ Container.propTypes = {
 
 export const mapStateToProps = (state, ownProps) => ({
   state: state,
+  ownProps: ownProps,
   groupMenu: getGroupMenuState(ownProps),
   verseCheck: getVerseCheckState(ownProps),
   translationHelps: getTranslationHelpsState(ownProps),
