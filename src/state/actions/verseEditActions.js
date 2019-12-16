0398a6d0d763248e1bbfeeb849e555596d5ccc22
mannycolon <colonmanuel7@gmail.com>
2019-12-16 18:44:53 -0500

    Made progress w/ verseEditActions.js

--- src/state/actions/verseEditActions.js
+++ src/state/actions/verseEditActions.js
@@ -5,22 +5,20 @@ import { getGatewayLanguageCodeAndQuote } from '../../helpers/gatewayLanguageHel
 import { getContextId } from '../../selectors';
 
 /**
- * This is a called by tool when a verse has been edited.  It updates group data reducer for current tool
- *   and updates file system for tools not loaded.
+ * This is a called by tool when a verse has been edited. It updates group data reducer for current tool
+ * and updates the file system for tools not loaded.
  * This will first do TW selections validation and prompt user if invalidations are found.
- * Then it calls updateVerseEditStatesAndCheckAlignments to saving verse edits and then validate alignments.
- *
+ * Then it calls updateVerseEditStatesAndCheckAlignments to save verse edits and then validate alignments.
  * @param {int} chapterWithVerseEdit
  * @param {int|string} verseWithVerseEdit
  * @param {string} before - the verse text before the edit
  * @param {string} after - the verse text after the edit
  * @param {string[]} tags - an array of tags indicating the reason for the edit
  */
-export const editTargetVerse = (chapterWithVerseEdit, verseWithVerseEdit, before, after, tags, gatewayLanguageCode, glBibles) => (dispatch, getState) => {
+export const editTargetVerse = (chapterWithVerseEdit, verseWithVerseEdit, before, after, tags, gatewayLanguageCode, glBibles, username) => (dispatch, getState) => {
   const state = getState();
   const contextId = getContextId(state);
   const currentCheckContextId = contextId;
-  const username = state.loginReducer.userdata.username;
   const { gatewayLanguageQuote } = getGatewayLanguageCodeAndQuote(gatewayLanguageCode, contextId, glBibles);
   const {
     bookId, chapter: currentCheckChapter, verse: currentCheckVerse,
@@ -35,13 +33,6 @@ export const editTargetVerse = (chapterWithVerseEdit, verseWithVerseEdit, before
       verse: verseWithVerseEdit,
     },
   };
-    // fallback to the current username
-  let userAlias = username;
-
-  if (userAlias === null) {
-    userAlias = getUsername(getState());
-  }
-
   const selectionsValidationResults = {};
   const actionsBatch = [];
 
@@ -54,11 +45,11 @@ export const editTargetVerse = (chapterWithVerseEdit, verseWithVerseEdit, before
     verseBefore: before,
     verseAfter: after,
     tags,
-    userName: userAlias,
+    username,
     activeBook: bookId,
     activeChapter: currentCheckChapter,
     activeVerse: currentCheckVerse,
-    modifiedTimestamp: modifiedTimestamp,
+    modifiedTimestamp,
     gatewayLanguageCode,
     gatewayLanguageQuote,
     contextId: contextIdWithVerseEdit,
