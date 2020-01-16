22f4079f51856898ebd219a21cbe578bae5cddc2
mannycolon <colonmanuel7@gmail.com>
2020-01-16 10:15:24 -0400

    Added saveGroupsData function to saveMethods.js

--- src/localStorage/saveMethods.js
+++ src/localStorage/saveMethods.js
@@ -2,7 +2,7 @@
 import fs from 'fs-extra';
 import path from 'path-extra';
 import generateTimestamp from '../utils/generateTimestamp';
-import { PROJECT_CHECKDATA_DIRECTORY } from '../common/constants';
+import { PROJECT_CHECKDATA_DIRECTORY, PROJECT_INDEX_FOLDER_PATH } from '../common/constants';
 
 /**
  * Generates the output directory.
@@ -84,6 +84,31 @@ export const saveBookmark = (contextId, bookmarkData, projectSaveLocation) => {
   } catch (err) {
     console.error(err);
     throw new Error(err);
+  }
+};
+
+/**
+ * Saves the groups data by groupId.
+ * @param {object} groupsData - groups Data.
+ * @param {string} projectSaveLocation - project directory path.
+ * @param {string} toolName - tool Name.
+ * @param {string} toolName - book Id.
+ */
+export const saveGroupsData = (groupsData, projectSaveLocation, toolName, bookId) => {
+  try {
+    if (groupsData && projectSaveLocation && toolName && bookId) {
+      for (const groupID in groupsData) {
+        if (groupsData[groupID]) {
+          const fileName = groupID + '.json';
+          const savePath = path.join(projectSaveLocation, PROJECT_INDEX_FOLDER_PATH, toolName, bookId, fileName);
+          fs.outputJsonSync(savePath, groupsData[groupID], { spaces: 2 });
+        }
+      }
+    } else {
+      console.error('saveGroupsData(): missing required arguments.');
+    }
+  } catch (err) {
+    console.error(err);
   }
 };
 
