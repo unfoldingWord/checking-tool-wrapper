f9c164fcd1457d124f1ee82bc37ea7c1f7cb2bf0
mannycolon <colonmanuel7@gmail.com>
2020-01-22 13:19:21 -0500

    Working on adding missing groupsDataReducer cases

--- src/state/reducers/groupsDataReducer.js
+++ src/state/reducers/groupsDataReducer.js
@@ -2,12 +2,14 @@ import isEqual from 'deep-equal';
 import _ from 'lodash';
 import {
   CLEAR_PREVIOUS_GROUPS_DATA,
+  SET_BOOKMARKS_IN_GROUPDATA,
+  SET_REMINDERS_IN_GROUPDATA,
+  SET_INVALIDATION_IN_GROUPDATA,
   LOAD_GROUPS_DATA_FROM_FS,
   TOGGLE_VERSE_EDITS_IN_GROUPDATA,
   TOGGLE_BOOKMARKS_IN_GROUPDATA,
   TOGGLE_COMMENTS_IN_GROUPDATA,
   TOGGLE_SELECTIONS_IN_GROUPDATA,
-  SET_INVALIDATION_IN_GROUPDATA,
   TOGGLE_MULTIPLE_VERSE_EDITS_IN_GROUPDATA,
 } from '../actions/actionTypes';
 import { getToggledGroupData } from '../../helpers/groupDataHelpers';
@@ -60,14 +62,6 @@ const groupsDataReducer = (state = initialState, action) => {
         [action.contextId.groupId]: getToggledGroupData(state, action, 'verseEdits'),
       },
     };
-  case SET_INVALIDATION_IN_GROUPDATA:
-    return {
-      ...state,
-      groupsData: {
-        ...state.groupsData,
-        [action.contextId.groupId]: getToggledGroupData(state, action, 'invalidated'),
-      },
-    };
   case TOGGLE_MULTIPLE_VERSE_EDITS_IN_GROUPDATA:
     return {
       ...state,
@@ -76,7 +70,23 @@ const groupsDataReducer = (state = initialState, action) => {
         [action.groupId]: setMultipleVerseEdits(state, action),
       },
     };
-  // TODO: Add missing toggle action cases.
+  case SET_INVALIDATION_IN_GROUPDATA:
+    return {
+      ...state,
+      groupsData: {
+        ...state.groupsData,
+        [action.contextId.groupId]: getToggledGroupData(state, action, 'invalidated'),
+      },
+    };
+  case SET_REMINDERS_IN_GROUPDATA:
+  case SET_BOOKMARKS_IN_GROUPDATA:
+    return {
+      ...state,
+      groupsData: {
+        ...state.groupsData,
+        [action.contextId.groupId]: getToggledGroupData(state, action, 'reminders'),
+      },
+    };
   case CLEAR_PREVIOUS_GROUPS_DATA:
     return initialState;
   default:
