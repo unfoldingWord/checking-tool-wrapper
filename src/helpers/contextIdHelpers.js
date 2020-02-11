56ade5f3999a698a70996e5c89b8d37404068915
mannycolon <colonmanuel7@gmail.com>
2020-02-11 18:33:56 -0400

    Addressing sameContext() question from PR review

--- src/helpers/contextIdHelpers.js
+++ src/helpers/contextIdHelpers.js
@@ -42,9 +42,12 @@ export const saveContextId = (contextId, projectSaveLocation) => {
  */
 export const sameContext = (contextId1, contextId2) => {
   if (contextId1 && contextId2) {
+    const isQuoteArray = Array.isArray(contextId1.quote);
+
     return isEqual(contextId1.reference, contextId2.reference) &&
       (contextId1.groupId === contextId2.groupId) &&
-      (contextId1.occurrence === contextId2.occurrence);
+      (contextId1.occurrence === contextId2.occurrence) &&
+      (isQuoteArray ? isEqual(contextId1.quote, contextId2.quote) : (contextId1.quote === contextId2.quote));
   }
   return false;
 };
