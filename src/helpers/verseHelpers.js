ef91f40ff05a0302355dfe756ea09fa9b6ba0552
mannycolon <colonmanuel7@gmail.com>
2020-02-20 10:27:58 -0400

    More debugging

--- src/helpers/verseHelpers.js
+++ src/helpers/verseHelpers.js
@@ -11,14 +11,29 @@ export function getVerseText(targetBible, contextId) {
   let verseText = '';
 
   if (contextId) {
+    console.log('====================================');
+    console.log('contextId', contextId);
+    console.log('====================================');
     const { chapter, verse } = contextId.reference;
+    console.log('====================================');
+    console.log('1 unfilteredVerseText', unfilteredVerseText);
+    console.log('====================================');
 
     if (targetBible && targetBible[chapter]) {
+      console.log('====================================');
+      console.log('targetBible', targetBible);
+      console.log('====================================');
       unfilteredVerseText = targetBible && targetBible[chapter] ? targetBible[chapter][verse] : '';
+      console.log('====================================');
+      console.log('2 unfilteredVerseText', unfilteredVerseText);
+      console.log('====================================');
 
       if (Array.isArray(unfilteredVerseText)) {
         unfilteredVerseText = unfilteredVerseText[0];
       }
+      console.log('====================================');
+      console.log('3 unfilteredVerseText', unfilteredVerseText);
+      console.log('====================================');
       // normalize whitespace in case selection has contiguous whitespace _this isn't captured
       verseText = normalizeString(unfilteredVerseText);
       verseText = usfmjs.removeMarker(verseText);
