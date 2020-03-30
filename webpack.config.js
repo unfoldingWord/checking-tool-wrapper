a004b389b5d14bc7dcbf759c37cd3aa722ff919e
Joel <joel@neutrinographics.com>
2020-03-30 16:06:24 +0700

    made tc-ui-toolkit external

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,7 @@ module.exports = {
   externals: {
     'react': 'commonjs react',
     'react-redux': 'react-redux',
+    'tc-ui-toolkit': 'tc-ui-toolkit'
   },
   resolve: { alias: { 'react-redux': path.resolve('./node_modules/react-redux') } },
 };
