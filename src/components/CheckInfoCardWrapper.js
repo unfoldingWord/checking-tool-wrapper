8c1a586384f5bf4e37038ec1cd440726c2d610ba
Joel <joel@neutrinographics.com>
2020-04-06 14:01:55 +0700

    fix

--- src/components/CheckInfoCardWrapper.js
+++ src/components/CheckInfoCardWrapper.js
@@ -40,6 +40,11 @@ function CheckInfoCardWrapper({
       // TRICKY: the translation helps wrapper requires a custom link format
       const link = href.replace(/rc:\/\/([^/]+)\/ta\/man\/([^/)]+)\/([^/)]+)/g, '$1/ta/$2/$3');
       window.followLink(link);
+
+      // TRICKY: open the helps so the modal mounts.
+      if (!showHelps) {
+        toggleHelps();
+      }
     }
   }
 
