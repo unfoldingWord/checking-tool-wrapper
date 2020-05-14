cd58a7b0e3deabf5e63a2ca59e5555e7ffbfe17d
Bruce McLean <mcleanb@photo-nomad.net>
2020-05-14 13:00:02 -0400

    changed saveData to async save

--- src/localStorage/saveMethods.js
+++ src/localStorage/saveMethods.js
@@ -121,6 +121,22 @@ export const saveVerseEdit = (contextId, verseEditData, projectSaveLocation) => 
 };
 
 /**
+ * save single group data item
+ * @param {object} groupsData - groups Data.
+ * @param {string} projectSaveLocation - project directory path.
+ * @param {string} toolName - tool Name.
+ * @param {string} bookId - book Id.
+ * @param {string} groupID - item to save
+ */
+export const saveGroupsDataItem = (groupsData, projectSaveLocation, toolName, bookId, groupID) => {
+  if (groupsData[groupID]) {
+    const fileName = groupID + '.json';
+    const savePath = path.join(projectSaveLocation, PROJECT_INDEX_FOLDER_PATH, toolName, bookId, fileName);
+    fs.outputJson(savePath, groupsData[groupID], { spaces: 2 }); // no need to wait for save to complete
+  }
+};
+
+/**
  * Saves the groups data by groupId.
  * @param {object} groupsData - groups Data.
  * @param {string} projectSaveLocation - project directory path.
@@ -131,11 +147,7 @@ export const saveGroupsData = (groupsData, projectSaveLocation, toolName, bookId
   try {
     if (groupsData && projectSaveLocation && toolName && bookId) {
       for (const groupID in groupsData) {
-        if (groupsData[groupID]) {
-          const fileName = groupID + '.json';
-          const savePath = path.join(projectSaveLocation, PROJECT_INDEX_FOLDER_PATH, toolName, bookId, fileName);
-          fs.outputJsonSync(savePath, groupsData[groupID], { spaces: 2 });
-        }
+        saveGroupsDataItem(groupsData, projectSaveLocation, toolName, bookId, groupID);
       }
     } else {
       console.error('saveGroupsData(): missing required arguments.');
