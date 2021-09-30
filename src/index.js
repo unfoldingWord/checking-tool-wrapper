4a15b40ccb480da3c0fdba61b0e9069f91699dba
Bruce McLean <mcleanb@photo-nomad.net>
2021-09-30 15:23:27 -0400

    fixes for verse spans

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export { default as Api } from './Api';
 export { default as Container } from './Container';
 export { default as reducers } from './state/reducers';
 export { getQuoteAsString } from './helpers/checkAreaHelpers';
+export { getAlignedTextFromBible } from './helpers/gatewayLanguageHelpers';
